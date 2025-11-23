d1a5d0e1f87b5f392436692296d286ff9911186d
Nikita Moshkin <nikitamoshkin1997@gmail.com>
2025-11-23 21:05:42 +0300

    Added basic laravel auth

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,14 @@
 <?php
 
+use App\Http\Controllers\Admin\TestController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
+Route::get('/', static function () {
     return view('welcome');
 });
 
-Route::group(['name' => 'Admin', 'prefix' => 'admin'], function() {
+Route::group(['name' => 'Admin', 'prefix' => 'admin'], static function() {
 
 });
+
+Auth::routes();
